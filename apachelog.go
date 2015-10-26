b863f822cbb8909158eb33deff922fa444aadf2b
Daisuke Maki <lestrrat+github@gmail.com>
2015-10-26 14:13:55 +0900

    Save path

--- apachelog.go
+++ apachelog.go
@@ -22,9 +22,14 @@ func NewWithWriterAndLogger(out io.Writer, logger *al.ApacheLog) gin.HandlerFunc
 	logger.SetOutput(out)
 	return func(c *gin.Context) {
 		t := time.Now()
+
+		// URL Paths *may* be modified, so keep it for later use
+		path := c.Request.URL.Path
+
 		// Process request
 		c.Next()
 
+		c.Request.URL.Path = path
 		logger.LogLine(c.Request, c.Writer.Status(), c.Writer.Header(), time.Since(t))
 	}
 }
